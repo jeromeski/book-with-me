f020651eb71405e6619ec95a7500555d1e939210
jeromeski <jeromegee@gmail.com>
2021-03-24 12:27:52 +0800

    Created and used RentalCard component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import RentalCard from './components/rental/RentalCard';
 import Header from './shared/Header'
 
 class App extends Component {
   render () {
     return (
-      <div>
-        <Header />
-      </div>
-    )
+			<div>
+				<Header />
+				<div className='container'>
+					<section id='rentalListing'>
+						<h1 className='page-title'>Your Home All Around the World</h1>
+						<div className='row'>
+							<RentalCard />
+							<RentalCard />
+							<RentalCard />
+						</div>
+					</section>
+				</div>
+			</div>
+		);
   }
 }
 
