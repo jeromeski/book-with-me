ba97157ae6fa5cfe2109a1f1363beaaf6fdaef77
jeromeski <jeromegee@gmail.com>
2021-03-25 11:05:43 +0800

    Initialized Redux in App

--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -1,71 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import RentalCard from './RentalCard';
 
 class RentalList extends Component {
 	constructor() {
 		super();
-		this.state = {
-			rentals: [
-				{
-					id: 1,
-					title: 'Central Apartment',
-					city: 'New York',
-					street: 'Times Sqaure',
-					category: 'apartment',
-					image: 'http://via.placeholder.com/350x250',
-					bedrooms: 3,
-					description: 'Very nice apartment',
-					dailyRate: 34,
-					shared: false,
-					createdAt: '24/12/2017'
-				},
-				{
-					id: 2,
-					title: 'Central Apartment 2',
-					city: 'San Francisco',
-					street: 'Main street',
-					category: 'condo',
-					image: 'http://via.placeholder.com/350x250',
-					bedrooms: 2,
-					description: 'Very nice apartment',
-					dailyRate: 12,
-					shared: true,
-					createdAt: '24/12/2017'
-				},
-				{
-					id: 3,
-					title: 'Central Apartment 3',
-					city: 'Bratislava',
-					street: 'Hlavna',
-					category: 'condo',
-					image: 'http://via.placeholder.com/350x250',
-					bedrooms: 2,
-					description: 'Very nice apartment',
-					dailyRate: 334,
-					shared: true,
-					createdAt: '24/12/2017'
-				},
-				{
-					id: 4,
-					title: 'Central Apartment 4',
-					city: 'Berlin',
-					street: 'Haupt strasse',
-					category: 'house',
-					image: 'http://via.placeholder.com/350x250',
-					bedrooms: 9,
-					description: 'Very nice apartment',
-					dailyRate: 33,
-					shared: true,
-					createdAt: '24/12/2017'
-				}
-			]
-		};
+		this.state = {};
 		// I am binding the context of 'this' to this class of this function.
 		// this.addRental = this.addRental.bind(this);
 	}
 
 	renderRentals() {
-		return this.state.rentals.map((rental, idx) => (
+		return this.props.rentals.map((rental, idx) => (
 			<RentalCard key={idx} colNum={'col-md-3 col-xs-6'} {...rental} />
 		));
 	}
@@ -81,4 +27,10 @@ class RentalList extends Component {
 	}
 }
 
-export default RentalList;
+const mapStateToProps = (state) => {
+	return {
+		rentals: state.rentals
+	};
+};
+
+export default connect(mapStateToProps)(RentalList);
--- src/shared/Header.js
+++ src/shared/Header.js
@@ -38,3 +38,47 @@ const Header = () => {
 };
 
 export default Header;
+
+/*
+
+<nav className='navbar navbar-dark navbar-expand-lg'>
+	<div className='container'>
+		<a className='navbar-brand' href=''>
+			BookWithMe
+		</a>
+		<form className='form-inline my-2 my-lg-0'>
+			<input
+				className='form-control mr-sm-2 bwm-search'
+				type='search'
+				placeholder='Try New York'
+				aria-label='Search'></input>
+			<button variant='btn-bwm-search' type='submit'>
+				<svg className='search-icon'>
+					<use xlinkHref={`${sprites}#icon-search`}></use>
+				</svg>
+			</button>
+		</form>
+		<button
+			className='navbar-toggler'
+			type='button'
+			data-toggle='collapse'
+			data-target='#navbarNavAltMarkup'
+			aria-controls='navbarNavAltMarkup'
+			aria-expanded='false'
+			aria-label='Toggle navigation'>
+			<span className='navbar-toggler-icon'></span>
+		</button>
+		<div className='collapse navbar-collapse' id='navbarNavAltMarkup'>
+			<div className='navbar-nav ml-auto'>
+				<a className='nav-item nav-link active' href=''>
+					Login <span className='sr-only'>(current)</span>
+				</a>
+				<a className='nav-item nav-link' href=''>
+					Register
+				</a>
+			</div>
+		</div>
+	</div>
+</nav>;
+
+*/ 
